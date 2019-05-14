df3c2f8b66e9c412c6575ba98e37dd9a448a928d
Kendrick Wilson <kwilson@bomgar.com>
2019-05-13 19:31:32 -0500

    updated to add install button

--- app-service-worker.js
+++ app-service-worker.js
@@ -1,7 +1,6 @@
 var CACHE_NAME = "cache-v1";
 
 // ----------------------------------------------------------------------
-/*
 self.addEventListener("install", (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
@@ -13,17 +12,15 @@ self.addEventListener("install", (e) => {
         })
     );
 });
-*/
 // ----------------------------------------------------------------------
-/*
 self.addEventListener("fetch", (e) => {
     e.respondWith(fetch(e.request).catch(() => {
         return caches.open(CACHE_NAME).then((cache) => {
             return caches.match(e.request);
         })
     }));
-}); 
-*/
+});
+
 // ----------------------------------------------------------------------
 var deferredPrompt;
 var installButton = document.getElementById("installButton");
