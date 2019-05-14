a4c2a5c7dee1667706e0ac7d7780f9c0c50355dc
Kendrick Wilson <kwilson@bomgar.com>
2019-05-14 11:24:03 -0500

    updated to fix issue with left text, restrict install button to chrome

--- app-controller.js
+++ app-controller.js
@@ -27,7 +27,7 @@ rightTextArea.addEventListener("focus", function(e) {
     rightPreview.innerHTML = text2html(rightTextArea.value);
 });
 leftTextArea.addEventListener("keyup", function(e) {
-    leftPreview.innerHTML = ltext2html(eftTextArea.value);
+    leftPreview.innerHTML = ltext2html(leftTextArea.value);
 });
 leftTextArea.addEventListener("focus", function(e) {
     leftPreview.innerHTML = text2html(leftTextArea.value);
@@ -68,6 +68,11 @@ function pullFromCache() {
         rightTextArea.value = '11:00 a.m. Service';
         buttomTextArea.value = 'this is a test\n\nthis is another test';
     }
+    if (leftTextArea.value == null && rightTextArea.value == null && buttomTextArea.value == null) {
+        leftTextArea.value = 'Produced By:\nMount Nebo Media\nMinistry';
+        rightTextArea.value = '11:00 a.m. Service';
+        buttomTextArea.value = 'this is a test\n\nthis is another test';
+    }
 
     rightPreview.innerHTML = text2html(rightTextArea.value);
     leftPreview.innerHTML = text2html(leftTextArea.value);
@@ -76,11 +81,70 @@ function pullFromCache() {
 }
 
 function text2html(texte) {
+    if (texte == null) {
+        return "";
+    } else {
+        texte = texte.replace(/\r/g, "");
+        texte = texte.replace(/\n/g, "<br />");
 
-    texte = texte.replace(/\r/g, "");
-    texte = texte.replace(/\n/g, "<br />");
-
-    return texte;
+        return texte;
+    }
 }
+
 // ----------------------------------------------------------------------------
 pullFromCache();
+
+// ----------------------------------------------------------------------------
+var objappVersion = navigator.appVersion;
+var objAgent = navigator.userAgent;
+var objbrowserName = navigator.appName;
+var objfullVersion = '' + parseFloat(navigator.appVersion);
+var objBrMajorVersion = parseInt(navigator.appVersion, 10);
+var objOffsetName, objOffsetVersion, ix;
+
+// In Chrome 
+if ((objOffsetVersion = objAgent.indexOf("Chrome")) != -1) {
+    objbrowserName = "Chrome";
+    objfullVersion = objAgent.substring(objOffsetVersion + 7);
+
+
+    // trimming the fullVersion string at semicolon/space if present 
+    if ((ix = objfullVersion.indexOf(";")) != -1) objfullVersion = objfullVersion.substring(0, ix);
+    if ((ix = objfullVersion.indexOf(" ")) != -1) objfullVersion = objfullVersion.substring(0, ix);
+    objBrMajorVersion = parseInt('' + objfullVersion, 10);
+    if (isNaN(objBrMajorVersion)) {
+        objfullVersion = '' + parseFloat(navigator.appVersion);
+        objBrMajorVersion = parseInt(navigator.appVersion, 10);
+    }
+
+    // In Chrome version 73 or greater
+    if (objBrMajorVersion >= 73) {
+        if ("serviceWorker" in navigator) {
+            navigator.serviceWorker.register("./app-service-worker.js")
+                .then(function(registration) { // Registration was successful
+                    console.log("ServiceWorker registration successful with scope: ", registration.scope);
+                    // ----------------------------------------------------------------------
+                    var deferredPrompt;
+                    window.addEventListener("beforeinstallprompt", function(e) {
+                        e.preventDefault();
+                        deferredPrompt = e;
+                        console.log("before install prompt fired");
+                        var installButton = document.getElementById("installButton");
+                        installButton.style.display = 'inline-block';
+                        installButton.addEventListener("click", function() {
+                            deferredPrompt.prompt();
+                            deferredPrompt.userChoice.then(function(choiceResult) {
+                                if (choiceResult.outcome === 'accepted') {
+                                    installButton.style.display = 'none';
+                                }
+                                deferredPrompt = null;
+                            })
+                        });
+                    });
+
+                }).catch(function(err) { // registration failed :(
+                    console.error("ServiceWorker registration failed: ", err);
+                });
+        }
+    }
+}
