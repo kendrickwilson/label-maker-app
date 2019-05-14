622ceeb48e37090a414fdc4d586fad825ba38410
Kendrick Wilson <kwilson@bomgar.com>
2019-05-13 19:50:26 -0500

    updated to add install button

--- app-service-worker.js
+++ app-service-worker.js
@@ -1,9 +1,9 @@
 var CACHE_NAME = "cache-v1";
 
 // ----------------------------------------------------------------------
-self.addEventListener("install", (e) => {
+self.addEventListener("install", function(e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
+        caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll(
                 [
                     "/favicon.png", "/index.html", "/app-controller.js", "/app-layout.css", "/app-service-worker.js"
@@ -13,9 +13,9 @@ self.addEventListener("install", (e) => {
     );
 });
 // ----------------------------------------------------------------------
-self.addEventListener("fetch", (e) => {
-    e.respondWith(fetch(e.request).catch(() => {
-        return caches.open(CACHE_NAME).then((cache) => {
+self.addEventListener("fetch", function(e) {
+    e.respondWith(fetch(e.request).catch(function() {
+        return caches.open(CACHE_NAME).then(function(cache) {
             return caches.match(e.request);
         })
     }));
